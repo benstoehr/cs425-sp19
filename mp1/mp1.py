3823474df85a23102f30c4f3d7766307e0626f0e
Benjamin Stoehr <benjaminxstoehr@gmail.com>
2019-02-17 21:46:14 -0500

    Ben's remake commit

--- mp1/mp1.py
+++ mp1/mp1.py
@@ -1,5 +1,201 @@
 # This is our mp1 code.
 # To run our chat client enter "python3 mp1.py <port> <number of participants>
 #
+# IMPORT STATEMENTS
 import socket
+import sys
+from threading import Thread
+import time
 
+
+
+
+#server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+IP_ADDR = ''
+
+# Addresses of other VMs for mp1
+VM0 = 'sp19-cs425-g32-01.cs.illinois.edu'
+VM1 = 'sp19-cs425-g32-02.cs.illinois.edu'
+VM2 = 'sp19-cs425-g32-03.cs.illinois.edu'
+VM3 = 'sp19-cs425-g32-04.cs.illinois.edu'
+VM4 = 'sp19-cs425-g32-05.cs.illinois.edu'
+VM5 = 'sp19-cs425-g32-06.cs.illinois.edu'
+VM6 = 'sp19-cs425-g32-07.cs.illinois.edu'
+VM7 = 'sp19-cs425-g32-08.cs.illinois.edu'
+VM_LIST = [VM0, VM1, VM2, VM3, VM4, VM5, VM6, VM7]
+
+#CUR_VM = 0
+#del VM_LIST[CUR_VM]
+
+
+# takes the first argument from command prompt: user name
+NAME = str(sys.argv[1])
+
+# takes the second argument from command prompt:  port number
+PORT = int(sys.argv[2])
+
+# takes the third argument from command prompt:  number of users
+USER_NUM = int(sys.argv[3])
+
+hostName = socket.gethostbyname()
+splitHostName = hostName.split("-")
+VM_NUMBER = splitHostName[3].split(".")[0]
+
+print("Running on VM: ", VM_NUMBER)
+
+class ServerSocket(Thread):
+
+    def __init__(self, sock=None, num_users=USER_NUM, ip=None, port=None):
+
+        super(ServerSocket, self).__init__()
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+        self.ip = ip
+        self.port = port
+
+        self.numberOfUsers = num_users - 1
+
+        self.connections = dict()
+        self.messageQueue = []
+        self.ready = False
+        self.activeConnections = 0
+
+
+    def bind(self, ip_addr, port):
+        # binds the server to IP and at the specified port number
+        self.sock.bind((ip_addr, port))
+        # listens for 8 active connections.
+        #self.sock.listen(8)
+        # listens for N active connections.
+        self.sock.listen(USER_NUM - 1)
+
+    def acceptConnections(self):
+
+        while(not self.ready):
+
+            # Accept a single connection
+            connection, address = self.sock.accept()
+
+            # Add connection to connection list
+            self.connectionList[address] = (connection, 'active')
+            print('# Server: Connection established by: ', address)
+            self.activeConnections += 1
+
+            # Once the proper number of connections is made, exit the while loop
+            if(self.activeConnections == (USER_NUM - 1)):
+                self.ready = True
+                print("Server: CONNECTED TO ALL THE CLIENTS!")
+
+            time.sleep(1)
+
+    def start(self):
+        self.bind(self.ip, self.port)
+        self.acceptConnections()
+
+    def run(self):
+
+        while(not self.ready):
+            print("NOT READY YET!")
+            time.sleep(1)
+
+        while(1):
+            # TODO: Main server logic
+            # iterate over each connection and read 8 bytes for message length
+            #
+            print("In the main loops")
+            continue
+
+
+
+class ClientSocket(Thread):
+
+    def __init__(self, sock=None, num_users=USER_NUM):
+        if sock is None:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+        self.connections = dict()
+        self.num_users = num_users - 1
+        self.activeConnections = 0
+
+        for i in range(num_users):
+            self.connections.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+
+    def connectToServers(self):
+
+        connectionStartTime = time.time()
+
+        servers = VM_LIST[:(self.num_users + 1)]
+        servers.remove(socket.gethostbyname())
+
+        attemptCount = 0
+        while(self.activeConnections == self.num_users):
+
+            curr_time = time.time()
+            if(curr_time - connectionStartTime > 30000):
+                print("30 second timeout for connecting to servers")
+                break
+
+            connectionIndex = attemptCount % self.num_users
+            server = servers[connectionIndex]
+
+            if(server in self.connections.keys()):
+                attemptCount += 1
+                continue
+
+            # Try connecting to servers
+            connectCheck = self.connections[i].connect((server, PORT))
+            if(connectCheck == -1):
+                print("Error connecting to server " + str(server))
+                self.connections[server] = 'inactive'
+                attemptCount += 1
+                continue
+            else:
+                self.activeConnections += 1
+                self.connections[server] = 'active'
+                attemptCount += 1
+                continue
+
+        # Check to ensure all the connections are made
+        if(self.activeConnections == self.num_users):
+            print("Connected to " + str(self.num_users) + " servers!")
+            print("I'M READY!")
+        else:
+            print("Error connecting to servers! SORRY")
+
+
+    def sendMessage(self, msg):
+
+        ts = [1, 0, 0, 0]
+        ts_byte = ','.join(ts).encode('utf-8')
+
+        for i in range():
+        self.sock.send(ts_byte)
+        self.sock.recv(1024)
+        self.sock.send(msg.encode('utf-8'))
+        self.sock.recv(1024)
+
+
+
+
+# Start the Server thread
+server = ServerSocket(num_users=USER_NUM)
+
+# Start the client
+client = ClientSocket(num_users=USER_NUM)
+
+
+server.bind(IP_ADDR, PORT)
+# TODO: Change to a looping while to accept all connections
+# include timeout from start of program
+server.acceptConnections()
+
+# TODO: Main logic loop
+# Every second look for message from all nodes, if missing, mark node as a failure
+
+# TODO:
