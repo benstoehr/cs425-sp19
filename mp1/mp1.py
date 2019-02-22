a6d2c1e2d659b2f49d4160f654c24137ca8f0008
Benjamin Stoehr <benjaminxstoehr@gmail.com>
2019-02-21 18:14:30 -0600

    Fixed init

--- mp1/mp1.py
+++ mp1/mp1.py
@@ -244,11 +244,8 @@ class ServerSocket(Thread):
 
                 if(status == 'active' and out_connection is not None):
 
-                    #print("{}: {}".format(hostname, status))
-                    #print("\tBeginning of Loop mes2send " + str(mes2send))
-
                     if (len(mes2send) > 0):
-
+                        print("{}: {}".format(hostname, status))
                         print("sending messages from queue " + str(mes2send))
                         print(str(self.hostname) + " -> " + str(hostname) + ": " + str(mes2send))
                         for m in mes2send:
@@ -308,7 +305,7 @@ class ServerSocket(Thread):
                 #print("\tEnd of loop mes2send " +str(mes2send))
                 self.connections[address] = (hostname, in_connection, out_connection, status, mes2send[:], sent_mes[:])
 
-            time.sleep(2)
+            #time.sleep(2)
             count = 0
 
         self.shutdown()
