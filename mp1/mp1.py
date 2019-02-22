98566d2ad8ca7fd3eaf5f6f04be8fbcefe0a3855
Benjamin Stoehr <benjaminxstoehr@gmail.com>
2019-02-21 19:05:51 -0600

    Fixed init

--- mp1/mp1.py
+++ mp1/mp1.py
@@ -258,12 +258,11 @@ class ServerSocket(Thread):
                 if(status == 'active' and out_connection is not None):
 
                     if (len(mes2send) > 0):
-                        print("{}: {}".format(hostname, status))
-                        print("\tsending messages from queue " + str(mes2send))
+                        #print("{}: {}".format(hostname, status))
+                        #print("\tsending messages from queue " + str(mes2send))
                         #print(str(self.hostname) + " -> " + str(hostname) + ": " + str(mes2send))
                         for m in mes2send:
                             # print(m)
-
                             out_connection.send(m)
                             sent_mes += [m]
                         self.connections[address] = (hostname, in_connection, out_connection, status, mes2send, sent_mes[:])
@@ -296,7 +295,7 @@ class ServerSocket(Thread):
                                 message = in_connection.recv(messageLength)
                                 fullReceivedMessage += message
 
-                                print("\tReceived message from " +str(hostname)+": " + message)
+                                #print("\tReceived message from " +str(hostname)+": " + message)
 
                                 if(fullReceivedMessage not in sent_mes):
                                     print(message)
