78323204248f4bea9056a86ba65e1a3fd2c8436b
Benjamin Stoehr <benjaminxstoehr@gmail.com>
2019-03-26 11:18:16 -0500

    Logging beginning

--- mp2/server.py
+++ mp2/server.py
@@ -110,6 +110,7 @@ class mp2Server(object):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         #self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.sock.bind((self.ip, self.port))
+        self.sock.settimeout(0.25)
 
 ################################
     def shutdown(self):
