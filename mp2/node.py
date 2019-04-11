d33388aa6081bfa8b701b6456ae51a3fb7ad5ee8
Benjamin Stoehr <benjaminxstoehr@gmail.com>
2019-04-11 10:40:58 -0500

    0.4.1

--- mp2/node.py
+++ mp2/node.py
@@ -213,10 +213,10 @@ class Node(Thread):
 
     def okToSendBlock(self, blockString, ip, port):
         if(blockString not in self.sentAddressesByBlock.keys()):
-            self.sentAddressesByBlock[blockString] = []
+            self.sentAddressesByBlock[blockString] = [(self.ip, self.port)]
 
         if(blockString not in self.receivedAddressesByBlock.keys()):
-            self.receivedAddressesByBlock[blockString] = []
+            self.receivedAddressesByBlock[blockString] = [(self.ip, self.port)]
 
         if((ip,int(port)) not in self.sentAddressesByBlock[blockString]):
             if((ip, int(port)) not in self.receivedAddressesByBlock[blockString]):
