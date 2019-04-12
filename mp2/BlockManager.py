5f11e271cf70c8b493beb49c80c3c533d0f49f99
claire <amoss.claire@gmail.com>
2019-04-12 15:30:11 -0500

    try minTx=5 & tx rate=5

--- mp2/BlockManager.py
+++ mp2/BlockManager.py
@@ -45,8 +45,8 @@ class BlockManager(object):
         self.waitingForBlockChainFrom = None
 
         ## MAX = 90
-        self.minTransactionsBeforeHash = 10
-        self.maxTransactionsBeforeHash = 15
+        self.minTransactionsBeforeHash = 5
+        self.maxTransactionsBeforeHash = 10
         self.numTransactionsBeforeHash = random.randint(self.minTransactionsBeforeHash, self.maxTransactionsBeforeHash)
 
 #############
