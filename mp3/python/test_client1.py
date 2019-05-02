1d5a81ac6a8d24b93a3f15fa55bd41fca04f04b0
claire <amoss.claire@gmail.com>
2019-05-02 10:32:30 -0500

    coordinator client test

--- mp3/python/test_client1.py
+++ mp3/python/test_client1.py
@@ -21,6 +21,26 @@ import grpc
 import mp3_pb2
 import mp3_pb2_grpc
 
+coordinatorVM = 'sp19-cs425-g58-10.cs.illinois.edu'
+
+channel = grpc.insecure_channel(str(coordinatorVM)+':50052')
+server = mp3_pb2_grpc.CoordinatorStub(channel)
+
+response = server.SayHi(mp3_pb2.HelloRequest(name='test1'))
+print("Coordinator client received: " + response.message)
+
+    try:
+        with sys.stdin as i:
+            while(1):
+                command = input()
+
+                checkreply = server.checkLock(mp3_pb2.checkMessage(name="test1", message=str(command)))
+                print(checkreply.message)
+
+    except KeyboardInterrupt:
+        exit(1)
+
+
 # serverVMs = ['[::]:50051',
 #  'sp19-cs425-g58-01.cs.illinois.edu',
 #  'sp19-cs425-g58-02.cs.illinois.edu',
@@ -30,70 +50,70 @@ import mp3_pb2_grpc
 
 # serverLetters = ['A', 'B', 'C', 'D', 'E']
 
-serverVMs = [
- 'sp19-cs425-g58-10.cs.illinois.edu',
- 'sp19-cs425-g58-09.cs.illinois.edu',
- 'sp19-cs425-g58-08.cs.illinois.edu',
- 'sp19-cs425-g58-07.cs.illinois.edu']
+# serverVMs = [
+#  'sp19-cs425-g58-10.cs.illinois.edu',
+#  'sp19-cs425-g58-09.cs.illinois.edu',
+#  'sp19-cs425-g58-08.cs.illinois.edu',
+#  'sp19-cs425-g58-07.cs.illinois.edu']
 
-serverLetters = ['A', 'B', 'C', 'D']
+# serverLetters = ['A', 'B', 'C', 'D']
 
-def run(numVMs):
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
+# def run(numVMs):
+#     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
+#     # used in circumstances in which the with statement does not fit the needs
+#     # of the code.
 
-    serverDict = dict()
+#     serverDict = dict()
 
-    for i in range(numVMs):
-        channel = grpc.insecure_channel(str(serverVMs[i])+':50051')
-        serverDict[serverLetters[i]] = mp3_pb2_grpc.GreeterStub(channel)
+#     for i in range(numVMs):
+#         channel = grpc.insecure_channel(str(serverVMs[i])+':50051')
+#         serverDict[serverLetters[i]] = mp3_pb2_grpc.GreeterStub(channel)
 
-        response = serverDict[serverLetters[i]].SayHello(mp3_pb2.HelloRequest(name='you'))
-        print("Greeter client received: " + response.message)
+#         response = serverDict[serverLetters[i]].SayHello(mp3_pb2.HelloRequest(name='you'))
+#         print("Greeter client received: " + response.message)
 
-        # bensname = serverDict[serverLetters[i]].getValue(mp3_pb2.getMessage(name="BensMac", key='A.x'))
-        # print("getValue received: " + bensname.message)
+#         # bensname = serverDict[serverLetters[i]].getValue(mp3_pb2.getMessage(name="BensMac", key='A.x'))
+#         # print("getValue received: " + bensname.message)
 
-    try:
-        with sys.stdin as i:
-            while(1):
-                command = input()
-
-
-                if('BEGIN' in command):
-                    for server in serverDict.values():
-                        beginreply = server.begin(mp3_pb2.beginMessage(name="Client1"))
-                        print(beginreply.message)
-
-                if('COMMIT' in command):
-                    for server in serverDict.values():
-                        commitreply = server.commit(mp3_pb2.beginMessage(name="Client1"))
-                        print(commitreply.message)
-
-                if('ABORT' in command):
-                    pass
-
-                if('GET' in command):
-                    get, serverkey = command.split(" ")
-                    server, key = serverkey[:].split(".")
-                    print(serverkey)
-
-                    getreply = serverDict[server].getValue(mp3_pb2.getMessage(name="Client1", serverkey=str(serverkey)))
-                    print(getreply.message)
-
-                if ('SET' in command):
-                    set, serverkey, value = command.split(" ")
-                    server, key = serverkey.split(".")
-
-                    setreply = serverDict[server].setValue(mp3_pb2.setMessage(name="Client1", serverkey=serverkey, value=value))
-                    print(setreply.message)
+#     try:
+#         with sys.stdin as i:
+#             while(1):
+#                 command = input()
 
 
-                #print(command)
+#                 if('BEGIN' in command):
+#                     for server in serverDict.values():
+#                         beginreply = server.begin(mp3_pb2.beginMessage(name="Client1"))
+#                         print(beginreply.message)
 
-    except KeyboardInterrupt:
-        exit(1)
+#                 if('COMMIT' in command):
+#                     for server in serverDict.values():
+#                         commitreply = server.commit(mp3_pb2.beginMessage(name="Client1"))
+#                         print(commitreply.message)
+
+#                 if('ABORT' in command):
+#                     pass
+
+#                 if('GET' in command):
+#                     get, serverkey = command.split(" ")
+#                     server, key = serverkey[:].split(".")
+#                     print(serverkey)
+
+#                     getreply = serverDict[server].getValue(mp3_pb2.getMessage(name="Client1", serverkey=str(serverkey)))
+#                     print(getreply.message)
+
+#                 if ('SET' in command):
+#                     set, serverkey, value = command.split(" ")
+#                     server, key = serverkey.split(".")
+
+#                     setreply = serverDict[server].setValue(mp3_pb2.setMessage(name="Client1", serverkey=serverkey, value=value))
+#                     print(setreply.message)
+
+
+#                 #print(command)
+
+#     except KeyboardInterrupt:
+#         exit(1)
 
 
 # serverVMs = ['sp19-cs425-g58-10.cs.illinois.edu',
