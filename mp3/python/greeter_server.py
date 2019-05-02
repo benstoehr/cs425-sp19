5204f147e11d793c91f70ead6eb38a5258ecccc0
Benjamin Stoehr <benjaminxstoehr@gmail.com>
2019-05-02 12:50:50 -0500

    Coordinator

--- mp3/python/greeter_server.py
+++ mp3/python/greeter_server.py
@@ -346,7 +346,9 @@ if __name__ == '__main__':
     lockDict = dict()
     #waitDict = dict()
 
-    coordinatorChannel = grpc.insecure_channel('sp19-cs425-g58-03.cs.illinois.edu:50052')
+
+    coordinatorChannel = grpc.insecure_channel('10.193.240.202:50052')\
+    #coordinatorChannel = grpc.insecure_channel('sp19-cs425-g58-03.cs.illinois.edu:50052')
     coordinator = mp3_pb2_grpc.CoordinatorStub(coordinatorChannel)
 
     #d['A.x'] = 'Benjamin'
